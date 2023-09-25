2bbacdb8a51ba26bd4b58a63bb75b057dd9ef3ae
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-25 14:29:36 -0700

    Create user, message models

---
+++ models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true },
+  time_stamp: { type: String, required: true },
+  text: { type: Date, required: true, default: Date.now },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
