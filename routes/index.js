5790a0244c6e6cc3a671d602764056e3a643c883
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-26 11:29:50 -0700

    Add sign up get route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const signUpController = require('../controllers/signUpController');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/sign-up', signUpController.signup_get);
 
 module.exports = router;
