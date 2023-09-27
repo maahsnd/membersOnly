f5850b4c49c45f5f661fed3a5585a021e72ca1b7
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-27 10:07:10 -0700

    Implement home page message display

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,21 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const signUpController = require('../controllers/signUpController');
 const User = require('../models/User');
+const Message = require('../models/Message');
+const asyncHandler = require('express-async-handler');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', { title: 'Home', user: req.user });
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const message_list = await Message.find({}).sort({ time_stamp: 1 }).exec();
+    res.render('index', {
+      title: 'Home',
+      user: req.user,
+      message_list: message_list
+    });
+  })
+);
 
 router.get('/sign-up', signUpController.signup_get);
 
