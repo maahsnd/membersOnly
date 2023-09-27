55d15178fbfb9e390baff0f2cd8a856ee39a124b
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-27 16:04:11 -0700

    Remove unneeded requires

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const signUpController = require('../controllers/signUpController');
 const messageController = require('../controllers/messageController');
-const User = require('../models/User');
 const Message = require('../models/Message');
 const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
 
 /* GET home page. */
 router.get(
@@ -39,52 +35,18 @@ router.get('/:id/secret-password', signUpController.secret_get);
 
 router.post('/:id/secret-password', signUpController.secret_post);
 
-router.get('/log-in', function (req, res, next) {
-  res.render('log-in', { title: 'Log in' });
-});
+router.get('/:id/admin', signUpController.admin_get);
 
-router.post(
-  '/log-in',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/log-in'
-  })
-);
+router.post('/:id/admin', signUpController.admin_post);
+
+router.get('/:id/user-page', signUpController.user_get);
+
+router.get('/log-in', signUpController.login_get);
+
+router.post('/log-in', signUpController.login_post);
 
 router.get('/new-message', messageController.message_get);
 
 router.post('/new-message', messageController.message_post);
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({
-        username: username
-      });
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
 
 module.exports = router;
